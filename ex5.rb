6fa0f110717feddfe7b0968e9d938d5f50a70adf
David Yip <dvd.yip@gmail.com>
2014-01-15 10:16:01 -0500

    used to_f instead of adding a point

--- ex5.rb
+++ ex5.rb
@@ -1,8 +1,8 @@
 #David Yip
-#I learned that to do a floating point op you need to add a decimal to the number.
+#I learned that to do a floating point op you need to add a decimal to the number or use to_f
 
 def to_celsius(deg_f)
-	deg_c = (deg_f - 32) * (5.0/9.0)
+	deg_c = (deg_f - 32) * (5.to_f/9.to_f)
 end
 
 puts "Input Fahrenheit temperature:"
