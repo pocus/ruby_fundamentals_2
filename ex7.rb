785e77a60bc109781d3f1a8cff1c033bc3beb308
David Yip <dvd.yip@gmail.com>
2014-01-15 12:35:34 -0500

    done steps 1-4

---
+++ ex7.rb
@@ -0,0 +1,17 @@
+#David Yip
+
+cohort_size = {:cohort1 => 34 , :cohort2 => 42, :cohort3 => 22}
+
+def print_cohort(cohort_size)
+	cohort_size.each{|key, val| puts "#{key}: #{val} students"}
+end
+
+print_cohort(cohort_size)
+
+cohort_size[:cohort4] = 43
+puts cohort_size.keys 
+
+cohort_size.each{ |key, val| cohort_size[key] = val * 1.05}
+
+puts "\nAfter increase"
+print_cohort(cohort_size)
